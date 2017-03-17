96cb7ead1816ea39dd9ac06bd31db34cc25bb9b6
Catherine Arnould <catherinearnould@MacBook-Pro-de-Kat.local>
2017-03-17 11:48:20 +0100

    test change structure

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,7 +1,7 @@
 import CMS from "netlify-cms";
-import PostPreview from "./cms-preview-templates/post";
+import PostPreview from "./cms-preview-templates/work";
 import ProductsPreview from "./cms-preview-templates/products";
 
 CMS.registerPreviewStyle("/css/main.css");
-CMS.registerPreviewTemplate("post", PostPreview);
+CMS.registerPreviewTemplate("work", WorkPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
